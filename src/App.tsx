39394342636c6175dbf658db6b29cdd79182b34a
AleGarciaBernal <aleandy7@gmail.com>
2024-04-24 21:48:24 -0400

    Merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Main from "./components/Main";
 
 function App() {
   return (
     <>
       <Register />
+      <Main/>
     </>
   );
 }
