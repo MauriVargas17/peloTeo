db5192e2cbc8bfb1c364383dec07643be2fb05dd
AleGarciaBernal <aleandy7@gmail.com>
2024-05-06 08:54:57 -0400

    Activity Info connected to BE

---
+++ src/components/Mapa.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+interface MapaProps {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+const Mapa: React.FC<MapaProps> = ({ location }) => {
+  return (
+    <LoadScript googleMapsApiKey="API-KEY">
+      <GoogleMap
+        mapContainerStyle={{ width: '100%', height: '350px' }}
+        center={location}
+        zoom={15}
+      >
+        <Marker position={location} />
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default Mapa;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
+import React, { useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
-import { useState } from "react";
 import Main from "../components/Main";
 import { AdminPage } from "../pages/AdminPage";
 import Login from "../components/Login";
@@ -9,37 +9,37 @@ import { getReservations } from "../services/ReservationsService";
 import { ActivityPage } from "../pages/ActivityPage";
 
 export const AppRoutes = () => {
-    const [token, setToken] = useState("");
-    const [activity, setActivity] = useState("");
-    return(
-        <Routes>
-            <Route path="/login" element={ <Login setToken={setToken}/> } />
-            <Route path="/register" element={ <Register setToken={setToken}/> } />
-            <Route
-                path="/home"
-                element={
-                    <PrivateRoute token={token}>
-                        <Main />
-                    </PrivateRoute>
-                }
-            />
-            <Route
-                path="/admin_page"
-                element={
-                    <PrivateRoute token={token}>
-                        <AdminPage reservas={getReservations(token)}/>
-                    </PrivateRoute>
-                }
-            />
-             <Route
-                path="/details"
-                element={
-                    <PrivateRoute token={token}>
-                        <ActivityPage activity={activity}/>
-                    </PrivateRoute>
-                }
-            />
-            <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-    );
-}
+  const [token, setToken] = useState("");
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login setToken={setToken} />} />
+      <Route path="/register" element={<Register setToken={setToken} />} />
+      <Route
+        path="/home"
+        element={
+          <PrivateRoute token={token}>
+            <Main />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/admin_page"
+        element={
+          <PrivateRoute token={token}>
+            <AdminPage reservas={getReservations(token)} />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/details/:id" // Cambiado para recibir el ID de la actividad
+        element={
+          <PrivateRoute token={token}>
+            <ActivityPage />
+          </PrivateRoute>
+        }
+      />
+      <Route path="*" element={<Navigate to="/login" />} />
+    </Routes>
+  );
+};
