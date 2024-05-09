eee78d7e760881041056e05fa93b53d2216c0481
AleGarciaBernal <aleandy7@gmail.com>
2024-05-09 10:17:11 -0400

    Peloteo V.1.1

--- src/components/Mapa.tsx
+++ src/components/Mapa.tsx
@@ -10,7 +10,7 @@ interface MapaProps {
 
 const Mapa: React.FC<MapaProps> = ({ location }) => {
   return (
-    <LoadScript googleMapsApiKey="AIzaSyC2qZajToqbn_z2wcJOwaecPLSjhKJaLbM">
+    <LoadScript googleMapsApiKey="API-KEY">
       <GoogleMap
         mapContainerStyle={{ width: '100%', height: '350px' }}
         center={location}
